129851d8848e7f3192b49faab9700999df401d91
Paweł Prażak <pawelprazak@gmail.com>
2019-01-08 21:14:38 +0100

    use test.Run and test.Test in all tests

--- pkg/matcher/matcher_test.go
+++ pkg/matcher/matcher_test.go
@@ -1,173 +1,148 @@
 package matcher
 
 import (
-	"fmt"
 	"testing"
 
+	"github.com/VirtusLab/go-extended/pkg/test"
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_matcher_MatchGroups(t *testing.T) {
-	type fields struct {
-		matcher Matcher
-	}
-	type args struct {
-		value string
-	}
-	type test struct {
-		name   string
-		fields fields
-		args   args
-		want   map[string]string
-		wantOk bool
-		f      func(tt test)
-	}
-	standard := func(tt test) {
-		actual, ok := tt.fields.matcher.MatchGroups(tt.args.value)
-		assert.Equal(t, tt.wantOk, ok, "test: '%s', entry: '%s'", tt.name, tt.args.value)
-		assert.EqualValues(t, tt.want, actual, "test: '%s', entry: '%s'", tt.name, tt.args.value)
-	}
-	tests := []test{
-		{
-			name: "empty expression",
-			fields: fields{
-				matcher: Must(``),
+	test.Run(t,
+		test.Test{
+			Name: "empty expression",
+			Fn: func(tt test.Test) {
+				m := Must(``)
+				value := ""
+				want := map[string]string{}
+
+				got, ok := m.MatchGroups(value)
+
+				assert.True(t, ok, "test: '%s', entry: '%s'", tt.Name, value)
+				assert.EqualValues(t, want, got, "test: '%s', entry: '%s'", tt.Name, value)
 			},
-			args: args{
-				value: "",
-			},
-			want:   map[string]string{},
-			wantOk: true,
-			f:      standard,
 		},
-		{
-			name: "simple expression",
-			fields: fields{
-				matcher: Must(`^(?P<name>\S+)=(?P<value>\S*)$`),
+		test.Test{
+			Name: "simple expression",
+			Fn: func(tt test.Test) {
+				m := Must(`^(?P<name>\S+)=(?P<value>\S*)$`)
+				value := "test=something"
+				want := map[string]string{
+					"name":  "test",
+					"value": "something",
+				}
+
+				got, ok := m.MatchGroups(value)
+
+				assert.True(t, ok, "test: '%s', entry: '%s'", tt.Name, value)
+				assert.EqualValues(t, want, got, "test: '%s', entry: '%s'", tt.Name, value)
 			},
-			args: args{
-				value: "test=something",
-			},
-			want: map[string]string{
-				"name":  "test",
-				"value": "something",
-			},
-			wantOk: true,
-			f:      standard,
 		},
-		{
-			name: "git url expression - empty",
-			fields: fields{
-				matcher: Must(`^git@(?P<hostname>[\w\-\.]+):(?P<organisation>[\w\-]+)\/(?P<name>[\w\-]+)\.git$`),
+		test.Test{
+			Name: "git url expression - empty",
+			Fn: func(tt test.Test) {
+				m := Must(`^git@(?P<hostname>[\w\-\.]+):(?P<organisation>[\w\-]+)\/(?P<name>[\w\-]+)\.git$`)
+				value := ""
+				want := map[string]string{}
+
+				got, ok := m.MatchGroups(value)
+
+				assert.False(t, ok, "test: '%s', entry: '%s'", tt.Name, value)
+				assert.EqualValues(t, want, got, "test: '%s', entry: '%s'", tt.Name, value)
 			},
-			args: args{
-				value: "",
-			},
-			want:   map[string]string{},
-			wantOk: false,
-			f:      standard,
 		},
-		{
-			name: "git url expression - invalid",
-			fields: fields{
-				matcher: Must(`^git@(?P<hostname>[\w\-\.]+):(?P<organisation>[\w\-]+)\/(?P<name>[\w\-]+)\.git$`),
+		test.Test{
+			Name: "git url expression - invalid",
+			Fn: func(tt test.Test) {
+				m := Must(`^git@(?P<hostname>[\w\-\.]+):(?P<organisation>[\w\-]+)\/(?P<name>[\w\-]+)\.git$`)
+				value := "invalid"
+				want := map[string]string{}
+
+				got, ok := m.MatchGroups(value)
+
+				assert.False(t, ok, "test: '%s', entry: '%s'", tt.Name, value)
+				assert.EqualValues(t, want, got, "test: '%s', entry: '%s'", tt.Name, value)
 			},
-			args: args{
-				value: "invalid",
-			},
-			want:   map[string]string{},
-			wantOk: false,
-			f:      standard,
 		},
-		{
-			name: "git url expression - missing extension",
-			fields: fields{
-				matcher: Must(`^git@(?P<hostname>[\w\-\.]+):(?P<organisation>[\w\-]+)\/(?P<name>[\w\-]+)\.git$`),
+		test.Test{
+			Name: "git url expression - missing extension",
+			Fn: func(tt test.Test) {
+				m := Must(`^git@(?P<hostname>[\w\-\.]+):(?P<organisation>[\w\-]+)\/(?P<name>[\w\-]+)\.git$`)
+				value := "git@something.com:anorg/arepo"
+				want := map[string]string{}
+
+				got, ok := m.MatchGroups(value)
+
+				assert.False(t, ok, "test: '%s', entry: '%s'", tt.Name, value)
+				assert.EqualValues(t, want, got, "test: '%s', entry: '%s'", tt.Name, value)
 			},
-			args: args{
-				value: "git@something.com:anorg/arepo",
-			},
-			want:   map[string]string{},
-			wantOk: false,
-			f:      standard,
 		},
-		{
-			name: "git url expression - missing extension",
-			fields: fields{
-				matcher: Must(`^git@(?P<hostname>[\w\-\.]+):(?P<organisation>[\w\-]+)\/(?P<name>[\w\-]+)\.git$`),
+		test.Test{
+			Name: "git url expression - missing extension",
+			Fn: func(tt test.Test) {
+				m := Must(`^git@(?P<hostname>[\w\-\.]+):(?P<organisation>[\w\-]+)\/(?P<name>[\w\-]+)\.git$`)
+				value := "git@something.com:anorg/arepo.git"
+				want := map[string]string{
+					"hostname":     "something.com",
+					"organisation": "anorg",
+					"name":         "arepo",
+				}
+				got, ok := m.MatchGroups(value)
+
+				assert.True(t, ok, "test: '%s', entry: '%s'", tt.Name, value)
+				assert.EqualValues(t, want, got, "test: '%s', entry: '%s'", tt.Name, value)
 			},
-			args: args{
-				value: "git@something.com:anorg/arepo.git",
-			},
-			want: map[string]string{
-				"hostname":     "something.com",
-				"organisation": "anorg",
-				"name":         "arepo",
-			},
-			wantOk: true,
-			f:      standard,
 		},
-		{
-			name: "compile fail",
-			f: func(tt test) {
+		test.Test{
+			Name: "compile fail",
+			Fn: func(tt test.Test) {
 				assert.PanicsWithValue(t,
 					"regexp: Compile(`<?:[`): error parsing regexp: missing closing ]: `[`",
 					func() {
 						Must(`<?:[`)
 					},
+					tt.Name,
 				)
 			},
 		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) { tt.f(tt) })
-	}
+	)
 }
 
 func Test_matcher_Match(t *testing.T) {
-	type fields struct {
-		matcher Matcher
-	}
-	type args struct {
-		value string
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   bool
-	}{
-		{
-			name:   "empty",
-			fields: fields{matcher: Must("^[a-z]+[0-9]+")},
-			args: args{
-				value: "",
-			},
-			want: false,
-		}, {
-			name:   "simple match",
-			fields: fields{matcher: Must("^[a-z]+[0-9]+")},
-			args: args{
-				value: "asdf1234",
-			},
-			want: true,
-		},
-		{
-			name:   "no match",
-			fields: fields{matcher: Must("^[a-z]+[0-9]+")},
-			args: args{
-				value: "1234asdf",
-			},
-			want: false,
-		},
-	}
+	test.Run(t,
+		test.Test{
+			Name: "empty",
+			Fn: func(tt test.Test) {
+				m := Must("^[a-z]+[0-9]+")
+				value := ""
 
-	for i, tt := range tests {
-		t.Run(fmt.Sprintf("[%d] %s", i, tt.name), func(t *testing.T) {
-			m := tt.fields.matcher
-			got := m.Match(tt.args.value)
-			assert.Equal(t, tt.want, got, tt.name)
-		})
-	}
+				got := m.Match(value)
+
+				assert.False(t, got, tt.Name)
+			},
+		},
+		test.Test{
+			Name: "simple match",
+			Fn: func(tt test.Test) {
+				m := Must("^[a-z]+[0-9]+")
+				value := "asdf1234"
+				got := m.Match(value)
+
+				assert.True(t, got, tt.Name)
+
+			},
+		},
+		test.Test{
+			Name: "no match",
+			Fn: func(tt test.Test) {
+				m := Must("^[a-z]+[0-9]+")
+				value := "1234asdf"
+
+				got := m.Match(value)
+
+				assert.False(t, got, tt.Name)
+
+			},
+		},
+	)
 }
--- pkg/renderer/render_test.go
+++ pkg/renderer/render_test.go
@@ -3,58 +3,69 @@ package renderer
 import (
 	"testing"
 
+	"github.com/VirtusLab/go-extended/pkg/test"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRenderer_NamedRender_Empty(t *testing.T) {
-	t.Run("empty render", func(_ *testing.T) {
-		input := ""
-		expected := ""
+func TestRenderer_NamedRender(t *testing.T) {
+	test.Run(t,
+		test.Test{
+			Name: "empty render",
+			Fn: func(_ test.Test) {
+				input := ""
+				expected := ""
 
-		result, err := New().NamedRender("test", input)
+				result, err := New().NamedRender("test", input)
 
-		assert.NoError(t, err)
-		assert.Equal(t, expected, result)
-	})
-}
-
-func TestRenderer_NamedRender_Simple(t *testing.T) {
-	t.Run("simple render", func(_ *testing.T) {
-		input := `key: {{ .value }}
+				assert.NoError(t, err)
+				assert.Equal(t, expected, result)
+			},
+		},
+		test.Test{
+			Name: "simple render",
+			Fn: func(_ test.Test) {
+				input := `key: {{ .value }}
 something:
   nested: {{ .something.nested }}`
 
-		expected := `key: some
+				expected := `key: some
 something:
   nested: val`
 
-		params := map[string]interface{}{
-			"value":     "some",
-			"something": map[string]string{"nested": "val"},
-		}
+				params := map[string]interface{}{
+					"value":     "some",
+					"something": map[string]string{"nested": "val"},
+				}
 
-		result, err := New(WithParameters(params)).NamedRender("test", input)
+				result, err := New(WithParameters(params)).NamedRender("test", input)
 
-		assert.NoError(t, err)
-		assert.Equal(t, expected, result)
-	})
+				assert.NoError(t, err)
+				assert.Equal(t, expected, result)
+			},
+		},
+	)
 }
 
-func TestRenderer_Render_Error(t *testing.T) {
-	t.Run("parse error", func(_ *testing.T) {
-		input := "{{ wrong+ }}"
-		expected := ""
+func TestRenderer_Render(t *testing.T) {
+	test.Run(t,
+		test.Test{
+			Name: "parse error",
+			Fn: func(_ test.Test) {
+				input := "{{ wrong+ }}"
+				expected := ""
 
-		result, err := New().NamedRender("test", input)
+				result, err := New().NamedRender("test", input)
 
-		assert.Error(t, err)
-		assert.Equal(t, expected, result)
-	})
-}
-
-func TestRenderer_Render_Validate_Default(t *testing.T) {
-	t.Run("validation", func(_ *testing.T) {
-		err := New().Validate()
-		assert.NoError(t, err)
-	})
+				assert.Error(t, err)
+				assert.Equal(t, expected, result)
+			},
+		},
+		test.Test{
+			Name: "validation",
+			Fn: func(_ test.Test) {
+				err := New().Validate()
+				assert.NoError(t, err)
+			},
+		},
+	)
 }
